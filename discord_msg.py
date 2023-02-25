d6c90ff9f6fd1f3e9a495001e69505072f46934b
Arjun Pramod <118660983+ArjunPramod@users.noreply.github.com>
2023-02-25 06:13:29 +0530

    Updated source code (Discord bot updated)

--- discord_msg.py
+++ discord_msg.py
@@ -5,7 +5,7 @@ def send_message(name_1):
     name = 'member'
 
     if name == 'member':
-        TOKEN = 'MTA3ODczMDEyODk1NzM3ODY0Mg.GKxVSg.A_81vhLOSGnqOd84ChBnSG0Ce9Y0qL_Xs27bRM'
+        TOKEN = "MTA3ODczMDEyODk1NzM3ODY0Mg.GR2mdo.7npwXg8bvDRq3zDTfn6rvh_6RJv3J1rj14MDZM"
         status = 'member'
         intents = discord.Intents.default()
         client = discord.Client(intents=intents)
@@ -13,6 +13,6 @@ def send_message(name_1):
         @client.event
         async def on_ready():
             channel = client.get_channel(1078722332446707762)
-            await channel.send(f'A lab member has entered the lab: {name_1}')
+            await channel.send(f"{name_1} has entered the lab")
 
         client.run(TOKEN)
